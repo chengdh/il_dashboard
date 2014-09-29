5a96bb6b8706bc61580cb513c05902db6d803dd7
chengdh <cheng.donghui@gmail.com>
2014-09-29 11:53:46 +0800

    change unicorn config file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,8 +1,8 @@
 # define paths and filenames
 deploy_to = "~/app/il_dashboard"
 rails_root = "#{deploy_to}/current"
-pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file= "#{deploy_to}/shared/unicorn.sock"
+pid_file = "#{rails_root}/tmp/pids/unicorn.pid"
+socket_file= "#{rails_root}/tmp/sockets/unicorn.sock"
 log_file = "#{rails_root}/log/unicorn.log"
 err_log = "#{rails_root}/log/unicorn_error.log"
 old_pid = pid_file + '.oldbin'
